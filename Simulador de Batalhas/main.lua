0314b24a73f03deefe925ac557f7107764e0a48c
MarioABeppler <mario.a.beppler@gmail.com>
2024-01-21 19:40:10 -0300

    Create new challenge

---
+++ Simulador de Batalhas/main.lua
@@ -0,0 +1,32 @@
+--  Modificar UTF-8 no terminal
+os.execute("chcp 65001")
+
+-- Header
+print([[
+=====================================================================
+                _
+     _         | |
+    | | _______| |---------------------------------------------\
+    |:-)_______|==[]============================================>
+    |_|        | |---------------------------------------------/
+               |_|
+
+                -----------------------------------------
+
+                        ⚔ SIMULADOR DE BATALHA ⚔
+
+=====================================================================
+
+
+                Você empunha sua espada e se prepara para lutar.
+                            É hora da batalha!
+                            
+]])
+
+-- obter definição do jogador
+
+-- obter definição do monstro
+
+-- apresentar o monstro 
+
+-- começar o loop de batalha
